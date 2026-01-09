1ff1e975dc5fc7520545e6ec8cec4fe584a695df
Phan Trung Thuận <thuantk000@gmail.com>
2026-01-09 19:43:27 +0700

    update

--- read_pickle.py
+++ read_pickle.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import pickle
 
 # file_path = 'coool/baseline/video_Driver_State_Changed.pkl'
 # file_path = 'coool/baseline/video_track_id.pkl'
-file_path = 'coool/baseline/hazard_name_by_frame_blip2opt.pkl'
+file_path_1 = 'coool/v1/hazard_name_by_id_1.pkl'
+file_path_2 = 'coool/v1/hazard_name_by_id_2.pkl'
 
-df = pd.read_pickle(file_path)
-print(df['video_0001'])
+df1 = pd.read_pickle(file_path_1)
+df2 = pd.read_pickle(file_path_2)
+df = df1 | df2
+
+print(len(df1), len(df2), len(df))
+
+with open("hazard_name_by_id.pkl", "wb") as f:
+    pickle.dump(df, f, protocol=pickle.HIGHEST_PROTOCOL)
